81b2b2527693f5cee99ed29092cb4fa8c6c1053b
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-09-01 20:41:34 -0300

    question 1 done, start the answer of question 2

--- DSA_stuff/fundamentals_of_python_language_for_data_analysis_and_data_science/Chapter_13_project_and_exam_2/project2_retail_dataset.py
+++ DSA_stuff/fundamentals_of_python_language_for_data_analysis_and_data_science/Chapter_13_project_and_exam_2/project2_retail_dataset.py
@@ -1,11 +1,33 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("dados/dataset.csv");
 
-# print(df)
+# print(df[df["Cidade"] == "New York City"])
 
 print("Which city has the biggest products sale value from the 'Office Supplies' category?")
 print("------------------------")
-sale_values = df[["Valor_Venda", "Categoria"]]
 
-print(sale_values.groupby("Categoria").head())
+office_suplies_df = df[df["Categoria"] == 'Office Supplies']
+
+sale_values = office_suplies_df[["Valor_Venda", "Categoria", "Cidade"]]
+
+cities_summed = sale_values.groupby("Cidade")["Valor_Venda"].sum().idxmax()
+
+print(cities_summed)
+
+# print(sale_values.groupby("Cidade")["Valor_Venda"].sum())
+
+print("What's the total of sales by Order Date?")
+print("OBS: Show the result using a bar chart!")
+print("------------------------")
+
+sales_by_order_date = df.groupby("Data_Pedido")["Valor_Venda"].sum()
+order_date_by_date = df.groupby("Valor_Venda")["Data_Pedido"].sum()
+
+print(sales_by_order_date.to_list())
+print(order_date_by_date.to_list())
+
+plt.bar(sales_by_order_date.to_list(), order_date_by_date.to_list())
+
+plt.show()
