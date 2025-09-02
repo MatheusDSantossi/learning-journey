8043d17da4d86b2eb6ec13b3ea4fda49e7d65a16
MatheusDSantossi <matheus1819.santos20@gmail.com>
2025-09-02 20:34:23 -0300

    feat: exercises 3, 4 and 5 done

--- DSA_stuff/fundamentals_of_python_language_for_data_analysis_and_data_science/Chapter_13_project_and_exam_2/project2_retail_dataset.py
+++ DSA_stuff/fundamentals_of_python_language_for_data_analysis_and_data_science/Chapter_13_project_and_exam_2/project2_retail_dataset.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("dados/dataset.csv");
 
@@ -18,16 +19,66 @@ print(cities_summed)
 
 # print(sale_values.groupby("Cidade")["Valor_Venda"].sum())
 
-print("What's the total of sales by Order Date?")
+print("What's the total of sales by Order Date -> Year?")
 print("OBS: Show the result using a bar chart!")
 print("------------------------")
 
-sales_by_order_date = df.groupby("Data_Pedido")["Valor_Venda"].sum()
-order_date_by_date = df.groupby("Valor_Venda")["Data_Pedido"].sum()
+df["Data_Pedido"] = pd.to_datetime(df["Data_Pedido"], format="mixed")
+df.set_index('Data_Pedido', inplace=True)
 
-print(sales_by_order_date.to_list())
-print(order_date_by_date.to_list())
+# Month
+# sales_by_order_date = df["Valor_Venda"].resample("M").max()
+# Year
+sales_by_order_date = df["Valor_Venda"].resample("Y").max()
 
-plt.bar(sales_by_order_date.to_list(), order_date_by_date.to_list())
+# sales_by_order_date.plot(kind='bar', figsize=(10, 6))
+# plt.xlabel("Date")
+# plt.ylabel("Maximum Sales")
+# plt.title("Maximum Sales by Date")
+# plt.xticks(rotation=45)
+# plt.tight_layout()
+# plt.show()
 
+print("What's the total of sales by state?")
+print("OBS: Show the result using a bar chart!")
+print("------------------------")
+
+total_of_sale_by_state = df.groupby("Estado")["Valor_Venda"].sum()
+
+# total_of_sale_by_state.plot(kind="bar", figsize=(10, 6))
+# plt.xlabel("State")
+# plt.ylabel("Sales")
+# plt.title("Total of sales by state")
+# plt.show()
+
+print("What's the 10 cities with the biggest total of sales?")
+print("OBS: Show the result using a bar chart!")
+print("------------------------")
+
+total_sales_by_city = df.groupby("Cidade")["Valor_Venda"].sum().sort_values(ascending=False).head(10)
+
+print(total_sales_by_city)
+
+# total_sales_by_city.plot(kind="bar", figsize=(10, 6))
+# plt.xlabel("Cities")
+# plt.ylabel("Sales")
+# plt.title("The 10 biggest Sales Cities")
+# plt.show()
+
+print("What's the 10 cities with the biggest total of sales?")
+print("OBS: Show the result using a pie chart!")
+print("------------------------")
+
+# A simple function to transform the value into a percentage
+def absolute_value(val):
+    a = np.round(val / 100 * df.groupby("Segmento")["Valor_Venda"].sum(), 0)
+    return a
+
+biggest_total_sales_segment = df.groupby("Segmento")["Valor_Venda"].sum().sort_values(ascending=False)
+
+biggest_total_sales_segment.plot(kind="pie", figsize=(10, 6))
+# plt.pie(biggest_total_sales_segment, autopct=absolute_value)
+plt.xlabel("Segments")
+plt.legend(biggest_total_sales_segment.index.astype(str))
+plt.va
 plt.show()
