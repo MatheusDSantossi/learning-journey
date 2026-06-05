10cfbab3d549e464b0a9afb71952778c5bbe8dfc
MatheusDSantossi <matheus1819.santos20@gmail.com>
2026-06-05 11:55:59 -0300

    feat: acknowledgement fixed with some experiments

--- backend_studies/kafka-project/demo/src/main/java/com/example/kafka/demo/config/KafkaConfig.java
+++ backend_studies/kafka-project/demo/src/main/java/com/example/kafka/demo/config/KafkaConfig.java
@@ -1,5 +1,26 @@
 package com.example.kafka.demo.config;
 
-public class KafkaConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 
+import com.example.kafka.demo.dto.OrderCreatedEvent;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent> kafkaListenerContainerFactory(
+            ConsumerFactory<String, OrderCreatedEvent> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, OrderCreatedEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+
+        return factory;
+    }
 }
--- backend_studies/kafka-project/demo/src/main/java/com/example/kafka/demo/consumer/OrderConsumer.java
+++ backend_studies/kafka-project/demo/src/main/java/com/example/kafka/demo/consumer/OrderConsumer.java
@@ -1,6 +1,10 @@
 package com.example.kafka.demo.consumer;
 
 import com.example.kafka.demo.dto.OrderCreatedEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -8,24 +12,43 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class OrderConsumer {
+    private final Set<String> processedOrders = new HashSet<>();
+
     @KafkaListener(topics = "orders", groupId = "order-group")
-    public void consumer(ConsumerRecord<String, OrderCreatedEvent> record, Acknowledgment acknowledgment) {
-        System.out.println("----- MESSAGE RECEIVED -----");
-        System.out.println("Key: " + record.key());
-        System.out.println("Partition: " + record.partition());
-        System.out.println("Offset: " + record.offset());
-        System.out.println("Value: " + record.value());
+    public void consume(ConsumerRecord<String, OrderCreatedEvent> record, Acknowledgment acknowledgment) {
+        String orderId = record.value().getOrderId();
 
-        System.out.println("record.value().getOrderId(): " + record.value().getOrderId());
-        if ("ORD-2".equals(record.value().getOrderId())) {
-            try {
+        if (processedOrders.contains(orderId)) {
+            System.out.println(
+                    "Duplicate message detected: " + orderId);
 
-                throw new RuntimeException("Simulated failure");
-            } catch (RuntimeException error) {
-                // Handle the exception
-                System.out.println("Exception caught: " + error.getMessage());
-            }
+            acknowledgment.acknowledge();
+            return;
         }
+
+        if ("ORD-999".equals(orderId)) {
+            throw new RuntimeException("Simulated crash");
+        }
+
+        processedOrders.add(orderId);
+
+        // System.out.println("----- MESSAGE RECEIVED -----");
+        // System.out.println("Key: " + record.key());
+        // System.out.println("Partition: " + record.partition());
+        // System.out.println("Offset: " + record.offset());
+        // System.out.println("Value: " + record.value());
+
+        // System.out.println("record.value().getOrderId(): " +
+        // record.value().getOrderId());
+        // if ("ORD-2".equals(record.value().getOrderId())) {
+        // try {
+
+        // throw new RuntimeException("Simulated failure");
+        // } catch (RuntimeException error) {
+        // // Handle the exception
+        // System.out.println("Exception caught: " + error.getMessage());
+        // }
+        // }
 
         acknowledgment.acknowledge();
     }
